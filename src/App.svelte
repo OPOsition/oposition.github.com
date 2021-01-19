12b4acce40113fd816205531a39ded9a3945f039
OPOsition <jeanbotn@gmail.com>
2021-01-19 22:54:59 +0100

    remove useless auth

--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,5 @@
 <script>
   import Smoke from './Smoke.svelte';
-
-  let input;
 </script>
 
-{#if input === 'smoke2020'}
-  <Smoke />
-{:else}
-  <input bind:value={input} />
-{/if}
-
-<style>
-  input {
-    background-color: var(--bg-color);
-    color: var(--text-color);
-  }
-</style>
+<Smoke />
