33c0e47b8ac0c292ab182dbf272e95b6c69cc428
OPOsition <jeanbotn@gmail.com>
2021-01-19 22:37:54 +0100

    add

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,19 @@
 <script>
   import Smoke from './Smoke.svelte';
+
+  let input;
+  console.log('app ok');
 </script>
 
-<Smoke />
+{#if input === 'smoke2020'}
+  <Smoke />
+{:else}
+  <input bind:value={input} />
+{/if}
+
+<style>
+  input {
+    background-color: var(--bg-color);
+    color: var(--text-color);
+  }
+</style>
